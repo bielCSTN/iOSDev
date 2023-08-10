1f20c15ac4ceecbb6737a2ab785a130191bd8f2f
bielCSTN <108473315+bielCSTN@users.noreply.github.com>
2023-08-10 10:23:48 -0300

    Add files via upload

---
+++ Meus Gastos/Meus Gastos/Meus_GastosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meus_GastosApp.swift
+//  Meus Gastos
+//
+//  Created by Biel on 10/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Meus_GastosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
